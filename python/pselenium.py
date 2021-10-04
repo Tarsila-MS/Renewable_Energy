68a8520b842c82364735dc4668f373116b0a3ca9
antoniobarolo <antoniobarolog@gmail.com>
2021-10-04 12:53:34 -0300

    Update pselenium.py

--- python/pselenium.py
+++ python/pselenium.py
@@ -5,33 +5,14 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from banco.py import insertAno
-from banco.py import insertPais
-from banco.py import insertDado
-
-def extrair_inteiro(texto):
-	try:
-		i = texto.rindex(' ')
-		sem_unidade = texto[:i]
-
-		# Às vezes, esse valor pode iniciar pelo ano...
-		i = sem_unidade.find(' ')
-		if i >= 0:
-			sem_unidade = sem_unidade[(i + 1):]
-
-		sem_virgula = sem_unidade.replace(',', '')
-
-		return int(sem_virgula)
-	except:
-		return 0
 
 driver = webdriver.Chrome()
 driver.get('https://ourworldindata.org/renewable-energy')
 
-botao_cookie = WebDriverWait(driver, 20).until(
-	EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[data-track-note="cookie-notice"]'))
-)
 
+botao_cookie = WebDriverWait(driver, 20).until(
+	EC.element_to_be_clickable((By.CSS_SELECTOR, 'button.button.accept'))
+)
 # Às vezes o botão não era clicável de verdade logo de primeira 😅 ...
 time.sleep(2)
 
@@ -40,7 +21,7 @@ botao_cookie.click()
 botoes_tabela = WebDriverWait(driver, 20).until(
 	EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'a[data-track-note="chart-click-table"]'))
 )
-
+# botoes_tabela[1].click()
 botoes_tabela[0].click()
 
 corpo_tabela = WebDriverWait(driver, 20).until(
@@ -49,7 +30,7 @@ corpo_tabela = WebDriverWait(driver, 20).until(
 
 linhas = corpo_tabela.find_elements_by_tag_name('tr')
 
-
+dados = []
 
 for linha in linhas:
 	colunas = linha.find_elements_by_tag_name('td')
@@ -57,12 +38,6 @@ for linha in linhas:
 	nome = colunas[0].text
 	valor1900 = colunas[1].text
 	valor2019 = colunas[2].text
-	dado = [nome, valor1900, linha['ano']]
-	insertAno(linhas['ano'])
-	insertPais(nome)
-	insertDado(dado)
+	print(nome, valor1900, valor2019)
 
-
-
-driver.close()
-#x path tabela -> /html/body/main/article/div[3]/div[2]/div/div/section[2]/div[2]/div[1]/figure/div/div[2]/div[2]/nav/ul/li[3]/a
+    
