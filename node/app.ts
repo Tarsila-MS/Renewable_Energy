6e24e670ee88d6fa257160302a1be78e678232be
Tarsila-MS <61888405+Tarsila-MS@users.noreply.github.com>
2021-11-01 12:42:24 -0300

    alteração das pastas

--- node/app.ts
+++ node/app.ts
@@ -1,21 +1,5 @@
 ﻿import app = require("teem");
-import appsettings = require("./appsettings");
 
 app.run({
-	root: appsettings.root,
-	staticRoot: "",
-	sqlConfig: appsettings.sqlPool,
-	htmlErrorHandler: function (err: any, req: app.Request, res: app.Response, next: app.NextFunction) {
-		res.status(err.status || 500);
-
-		if (req.path.indexOf("/api/") >= 0) {
-			res.json(err.status == 404 ? "Não encontrado" : (err.message || err.toString()));
-		} else if (err.status == 404) {
-			res.render("shared/erro", { layout: "layout-externo" });
-		} else {
-			// Como é um ambiente de desenvolvimento, deixa o objeto do erro
-			// ir para a página, que possivelmente exibirá suas informações
-			res.render("shared/erro", { layout: "layout-externo", mensagem: err.message, erro: err });
-		}
-    }
+    port: 3000
 });
