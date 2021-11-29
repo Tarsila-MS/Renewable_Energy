30da1e257411d4355543a48f8d9471f558718b19
antoniobarolo <antoniobarolog@gmail.com>
2021-11-29 12:09:26 -0300

    Update app.ts

--- node/app.ts
+++ node/app.ts
@@ -1,5 +1,14 @@
 ﻿import app = require("teem");
 
 app.run({
-    port: 3000
+    port: 3000,
+    sqlConfig: {
+        connectionLimit: 30,
+        charset: "utf8mb4",
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "root",
+        database: "renewableenergy"
+    }
 });
